6cf7d67e09bb883f79df10f8ded6d5c6eeaf11dc
Vinniko <vinnik_21@bk.ru>
2020-10-04 10:20:31 +0300

    Interfaces create commit
    Будет создан интерфейс IReader
    Будет создан интерфейс IRepository
    Будет создан интерфейс IWriter

---
+++ Core/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    interface IRepository
+    {
+        void Set<T>(T data);
+        object GetAll();
+        object GetByIndex(int index);
+        void Clear();
+        void Remove<T>(T obj);
+    }
+}
