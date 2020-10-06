8d8f421ae5d87fbc87a90ccc33b81f88f0f6c51c
Vinniko <vinnik_21@bk.ru>
2020-10-06 09:22:41 +0300

    AutofacConfig commit
    Будет создан класс AutofacConfig
    Будет добвлена реализация DI для Logger и Guardian

---
+++ SAUEP.TCPServer/Configs/AutofacConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using SAUEP.TCPServer.Interfaces;
+using SAUEP.TCPServer.Services;
+
+namespace SAUEP.TCPServer.Configs
+{
+    public sealed class AutofacConfig
+    {
+        public static IContainer ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Guardian>().SingleInstance();
+            builder.RegisterType<Logger>().As<ILogger>().SingleInstance();
+
+            return builder.Build();
+        }
+    }
+}
