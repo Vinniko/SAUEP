9384305ed30459b28c09a75b36a71d63dd1336d4
Vinniko <vinnik_21@bk.ru>
2020-10-06 15:44:08 +0300

    Create and add logic in SocketModel commit
    Будет создан класс-модель SocketModel с описанной логикой

---
+++ SAUEP.TCPServer/Models/SocketModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SAUEP.TCPServer.Models
+{
+    public sealed class SocketModel
+    {
+        #region Constructors
+
+        public SocketModel()
+        {
+            ListenSocket = new TcpListener(IPAddress.Parse(_ipAddress), _port);
+            ListenSocket.Start();
+        }
+
+        #endregion
+
+
+
+        #region Main Logic
+
+        public TcpClient GetSocket()
+        {
+            return ListenSocket.AcceptTcpClient();
+        }
+
+        #endregion
+
+
+
+        #region Destructors
+
+        ~SocketModel()
+        {
+            if (ListenSocket != null)
+                ListenSocket.Stop();
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        private int _port = 8005;
+        private string _ipAddress = "127.0.0.1";
+        public TcpListener ListenSocket { get; private set; }
+
+        #endregion
+    }
+}
