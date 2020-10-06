ab608ca028d8a7627a58582c9d0748daa3246c89
Vinniko <vinnik_21@bk.ru>
2020-10-06 10:01:40 +0300

    Create IRepository commit
    Будет создан интерфейс IRepository с методами: Set, GetAll, GetById, Update, Remove

---
+++ SAUEP.TCPServer/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAUEP.TCPServer.Interfaces
+{
+    interface IRepository
+    {
+        void Set<T>(T data);
+        ICollection<T> GetAll<T>();
+        T GetById<T>(int id);
+        void Update<T>(int id);
+        void Remove<T>(int id);
+    }
+}
