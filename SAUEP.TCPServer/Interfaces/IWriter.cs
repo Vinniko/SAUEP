be0147bf94157248d18f3a56ec063537ea72c445
Vinniko <vinnik_21@bk.ru>
2020-10-06 10:06:46 +0300

    Create IWriter commit
    Будет создан интерфейс IWriter с методом Write

---
+++ SAUEP.TCPServer/Interfaces/IWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAUEP.TCPServer.Interfaces
+{
+    interface IWriter
+    {
+        void Write<T>(T data);
+    }
+}
