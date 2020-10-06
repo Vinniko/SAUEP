3999a6666098dee3449611484f6e6e21e621ced9
Vinniko <vinnik_21@bk.ru>
2020-10-06 09:56:10 +0300

    Create IObservable commit
    Будет создан интерфейс IObservable с методами: AddObserver, RemoveObserver, NotifyObservers

---
+++ SAUEP.TCPServer/Interfaces/IObservable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAUEP.TCPServer.Interfaces
+{
+    interface IObservable
+    {
+        void AddObserver(IObserver observer);
+        void NotifyObservers();
+        void RemoveObserver(IObserver observer);
+    }
+}
