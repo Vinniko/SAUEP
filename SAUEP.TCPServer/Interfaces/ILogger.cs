310042a195cd2e93f5383b45ff3c7a98413e6cff
Vinniko <vinnik_21@bk.ru>
2020-10-06 09:21:52 +0300

    Add base services and interfaces commit
    Будет добавлен интерфейс ILogger
    Будет добавлен сервис Guardian
    Будет добавлен сервис Logger
    Будет добавлен сервис Result

---
+++ SAUEP.TCPServer/Interfaces/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAUEP.TCPServer.Interfaces
+{
+    public interface ILogger
+    {
+        void Logg(string text);
+    }
+}
---
+++ SAUEP.TCPServer/Services/Result.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAUEP.TCPServer.Services
+{
+    public class Result
+    {
+
+        public bool IsSuccessful { get; }
+        public Exception Exception { get; }
+
+        public static Result Successful() => new Result(true);
+        public static Result Failed(Exception exception) => new Result(false, exception);
+
+        protected Result(bool isSuccessful, Exception exception = null)
+        {
+            IsSuccessful = isSuccessful;
+
+            Exception = exception;
+        }
+
+        public static bool operator !(Result result) => !result.IsSuccessful;
+        public static bool operator true(Result result) => result.IsSuccessful;
+        public static bool operator false(Result result) => !result.IsSuccessful;
+    }
+
+    public sealed class Result<T> : Result
+    {
+        public T Value { get; }
+
+        protected Result(bool isSuccessful, Exception exception, T result) : base(isSuccessful, exception)
+        {
+            Value = result;
+        }
+
+        public static new Result Successful() => Successful(default(T));
+        public static Result<T> Successful(T value) => new Result<T>(true, null, value);
+
+        public static new Result<T> Failed(Exception exception) => new Result<T>(false, exception, default(T));
+    }
+}
