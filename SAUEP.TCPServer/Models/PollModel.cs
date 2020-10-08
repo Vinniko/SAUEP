a6aa3ef6f8498d2f88ca69b187e557c659dac86d
Vinniko <vinnik_21@bk.ru>
2020-10-08 15:02:46 +0300

    PollModel commit
    Будет создан класс PollModel

---
+++ SAUEP.TCPServer/Models/PollModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAUEP.TCPServer.Models
+{
+    public sealed class PollModel
+    {
+        public int Id { get; set; }
+    }
+}
