cc6ba7872a4fd3e467695530562c9c6b9321267e
Vinniko <vinnik_21@bk.ru>
2020-10-08 14:59:10 +0300

    Fix SocketModel commit
    Будут удалены лишние библиотеки

--- SAUEP.TCPServer/Models/SocketModel.cs
+++ SAUEP.TCPServer/Models/SocketModel.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.IO;
 
 namespace SAUEP.TCPServer.Models
 {
