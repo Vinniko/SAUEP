db09ac41cfd6464726a03c3560cae0fb186fcc4e
Vinniko <vinnik_21@bk.ru>
2020-10-16 11:00:59 +0300

    Create repository commit
    Будет создан и оформлен класс PollsRepository

--- SAUEP.TCPServer/Repositories/PollsRepository.cs
+++ SAUEP.TCPServer/Repositories/PollsRepository.cs
@@ -1,12 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SAUEP.TCPServer.Interfaces;
+using SAUEP.TCPServer.Models;
+using SAUEP.TCPServer.Connections;
+using SAUEP.TCPServer.Exceptions;
+using Npgsql;
 
 namespace SAUEP.TCPServer.Repositories
 {
-    class PollsRepository 
+    public sealed class PollsRepository : IRepository
     {
+        #region Constructors
+
+        public PollsRepository(IConnection connection)
+        {
+            _connection = connection;
+        }
+
+        #endregion
+
+
+
+        #region Main Logic
+
+        public void Set<T>(T data)
+        {
+            string query = String.Format("INSERT INTO Polls(deviceid, power, electricityConsumption, date) VALUES((SELECT Id FROM devices WHERE Serial = '{0}'), " +
+                "{1}, {2}, '{3}') ON CONFLICT DO NOTHING;",
+                (data as PollModel).Serial, (data as PollModel).Power, (data as PollModel).ElectricityConsumption,
+                (data as PollModel).Date);
+            using (var command = new NpgsqlCommand(query, (_connection as DataBaseConnection).Connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public ICollection<T> Get<T>()
+        {
+            ICollection<T> polls = new List<T>();
+            string allFromPollsQuery = "SELECT Polls.Id, Devices.Serial, Devices.Ip, Polls.Power, Polls.ElectricityConsumption, Polls.Date FROM Polls " +
+                "INNER JOIN Devices On Devices.Id = Polls.DeviceId;";
+            using (var command = new NpgsqlCommand(allFromPollsQuery, (_connection as DataBaseConnection).Connection))
+            {
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var poll = new PollModel(reader.GetInt32(0), reader.GetString(1), reader.GetString(2),
+                            reader.GetDouble(3), reader.GetDouble(4), reader.GetDateTime(5));
+                        (polls as List<PollModel>).Add(poll);
+                    }
+                    return polls;
+                }
+            }
+        }
+
+        public IModel GetById(int id)
+        {
+            string getJewelQuery = String.Format("SELECT Polls.Id, Devices.Serial, Devices.Ip, Polls.Power, Polls.ElectricityConsumption, Polls.Date FROM Polls " +
+                "INNER JOIN Devices On Devices.Id = Polls.DeviceId WHERE Polls.Id = {0};", id);
+            using (var command = new NpgsqlCommand(getJewelQuery, (_connection as DataBaseConnection).Connection))
+            {
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        var poll = new PollModel(reader.GetInt32(0), reader.GetString(1), reader.GetString(2),
+                            reader.GetDouble(3), reader.GetDouble(4), reader.GetDateTime(5));
+                        return poll;
+                    }
+                    else throw new DataBaseNullSelectException(String.Format("В таблице Jewel не существует записи с Id = {0}", id));
+                }
+            }
+        }
+
+        public void Update<T>(int id, T data)
+        {
+            string query = String.Format("UPDATE Polls SET deviceid = (SELECT Id FROM devices WHERE Serial = '{0}'), power = {1}, " +
+                "electricityConsumption = {2}, date = '{3}';",
+                (data as PollModel).Serial, (data as PollModel).Power, (data as PollModel).ElectricityConsumption,
+                (data as PollModel).Date);
+            using (var command = new NpgsqlCommand(query, (_connection as DataBaseConnection).Connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Remove<T>(int id)
+        {
+            string query = String.Format("DELETE FROM Polls WHERE Id = {0}", id);
+            using (var command = new NpgsqlCommand(query, (_connection as DataBaseConnection).Connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        private IConnection _connection;
+
+        #endregion
     }
 }
