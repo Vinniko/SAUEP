f8a0f086043a5153c060e4fc8eb93fb2508f9dba
Vinniko <vinnik_21@bk.ru>
2020-10-16 10:52:57 +0300

    Add services in autofacconfig commit
    В di регистрацию будет добавлена DataBaseConnection
    В di регистрацию будет добавлен PollsRepository
    В di регистрацию будет добавлен ConsoleWriter
    В di регистрацию будет добавлен JSONParser
    В di регистрацию будет добавлен SocketListener
    В di регистрацию будет добавлен SocketWriter

--- SAUEP.TCPServer/Configs/AutofacConfig.cs
+++ SAUEP.TCPServer/Configs/AutofacConfig.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autofac;
+﻿using Autofac;
 using SAUEP.TCPServer.Interfaces;
 using SAUEP.TCPServer.Services;
+using SAUEP.TCPServer.Repositories;
+using SAUEP.TCPServer.Connections;
 
 namespace SAUEP.TCPServer.Configs
 {
@@ -14,8 +11,15 @@ namespace SAUEP.TCPServer.Configs
         public static IContainer ConfigureContainer()
         {
             var builder = new ContainerBuilder();
+
             builder.RegisterType<Guardian>().SingleInstance();
             builder.RegisterType<Logger>().As<ILogger>().SingleInstance();
+            builder.RegisterType<DataBaseConnection>().As<IConnection>().SingleInstance();
+            builder.RegisterType<PollsRepository>().As<IRepository>().SingleInstance();
+            builder.RegisterType<ConsoleWriter>().As<IWriter>().SingleInstance();
+            builder.RegisterType<JSONParser>().As<IParser>().SingleInstance();
+            builder.RegisterType<SocketListener>().As<IListener>().SingleInstance();
+            builder.RegisterType<SocketWriter>().As<SocketWriter>().SingleInstance();
 
             return builder.Build();
         }
