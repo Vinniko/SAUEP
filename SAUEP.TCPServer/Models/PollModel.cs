dd3c8b015c7e7e01ba8c07b64578cd0a91e9bd26
Vinniko <vinnik_21@bk.ru>
2020-10-16 10:59:11 +0300

    Create model commit
    Будет создан класс PollModel
    Класс PollModel будет оформлен полями и конструктором

--- SAUEP.TCPServer/Models/PollModel.cs
+++ SAUEP.TCPServer/Models/PollModel.cs
@@ -1,13 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SAUEP.TCPServer.Interfaces;
 
 namespace SAUEP.TCPServer.Models
 {
-    public sealed class PollModel
+    public sealed class PollModel : IModel
     {
+        #region Constructors
+
+        public PollModel(int id, string serial, string ip, double power, double electricityConsumption, DateTime date)
+        {
+            Id = id;
+            Serial = serial;
+            Ip = ip;
+            Power = power;
+            ElectricityConsumption = electricityConsumption;
+            Date = date;
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
         public int Id { get; set; }
+        public string Serial { get; set; }
+        public string Ip { get; set; }
+        public double Power { get; set; }
+        public double ElectricityConsumption { get; set; }
+        public DateTime Date { get; set; }
+
+        #endregion
     }
 }
