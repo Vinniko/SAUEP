1ee70fdb038b1630866bcddffc758a106f88c7bc
Vinniko <vinnik_21@bk.ru>
2020-10-16 10:50:32 +0300

    Add end logic in main function commit
    Будет добавлена логика запуска сервера в многопотоковом режиме в главной функции Main

--- SAUEP.TCPServer/Program.cs
+++ SAUEP.TCPServer/Program.cs
@@ -6,73 +6,29 @@ using Autofac;
 using SAUEP.TCPServer.Configs;
 using SAUEP.TCPServer.Services;
 using SAUEP.TCPServer.Interfaces;
+using SAUEP.TCPServer.Models;
+using System.IO;
+using System.Threading;
 
 namespace SAUEP.TCPServer
 {
     public class Program
     {
-        public static void test()
-        {
-            int count = 0;
-            throw new Exception();
-        }
-        static int port = 8005; // порт для приема входящих запросов
         public static void Main(string[] args)
         {
             IContainer container = AutofacConfig.ConfigureContainer();
             using (var scope = container.BeginLifetimeScope())
             {
                 var guardian = scope.Resolve<Guardian>();
-                var k = scope.Resolve<ILogger>();
-                guardian.Secure(() => test());
-            }
-
-            
-            // получаем адреса для запуска сокета
-            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
-
-            // создаем сокет
-            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            try
-            {
-                // связываем сокет с локальной точкой, по которой будем принимать данные
-                listenSocket.Bind(ipPoint);
-
-                // начинаем прослушивание
-                listenSocket.Listen(10);
-
-                Console.WriteLine("Сервер запущен. Ожидание подключений...");
-
-                while (true)
-                {
-                    Socket handler = listenSocket.Accept();
-                    // получаем сообщение
-                    StringBuilder builder = new StringBuilder();
-                    int bytes = 0; // количество полученных байтов
-                    byte[] data = new byte[256]; // буфер для получаемых данных
-
-                    do
-                    {
-                        bytes = handler.Receive(data);
-                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                    }
-                    while (handler.Available > 0);
-
-                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
-
-                    // отправляем ответ
-                    string message = "ваше сообщение доставлено";
-                    data = Encoding.Unicode.GetBytes(message);
-                    handler.Send(data);
-                    // закрываем сокет
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                var listener = scope.Resolve<IListener>();
+                var socket = new SocketModel();
+                (listener as SocketListener).Socket = socket;
+                (listener as SocketListener).AddObserver(scope.Resolve<SocketWriter>());
+                var listen = new Thread(new ThreadStart(listener.Listen));
+                guardian.Secure(listen.Start);
+                listen.Join();
             }
         }
     }
+
 }
