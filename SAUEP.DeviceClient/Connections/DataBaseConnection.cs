83fd35d96277c3096a8cc3244ccc67d54804e7a3
Vinniko <vinnik_21@bk.ru>
2020-10-25 11:30:39 +0300

    Create DataBaseConnection class and add logic commit
    Будет создан класс DataBaseConnection
    Будут добавлены методы установки и разрыва соединения с базой данных

---
+++ SAUEP.DeviceClient/Connections/DataBaseConnection.cs
@@ -0,0 +1,37 @@
+﻿using SAUEP.TCPServer.Interfaces;
+using Npgsql;
+
+namespace SAUEP.DeviceClient.Connections
+{
+    public sealed class DataBaseConnection : IConnection
+    {
+        #region Constructors
+
+        public DataBaseConnection()
+        {
+            Connection = new NpgsqlConnection(_connection);
+            Connection.Open();
+        }
+
+        #endregion
+
+
+
+        #region Destructors
+        ~DataBaseConnection()
+        {
+            Connection.Close();
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        private const string _connection = "Host=localhost;Username=postgres;Password=123456789q;Database=SAUEPOLTP";
+        public NpgsqlConnection Connection { get; set; }
+
+        #endregion
+    }
+}
