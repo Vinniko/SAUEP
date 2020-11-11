75f46c14c96abcf11cde2fced75c5fb4bc850b94
Vinniko <vinnik_21@bk.ru>
2020-11-11 12:31:51 +0300

    Create FileReader in Srver and Device prj commit
    Будет создан сервер FileReader

---
+++ SAUEP.DeviceClient/Services/FileReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SAUEP.DeviceClient.Interfaces;
+using System.IO;
+
+namespace SAUEP.DeviceClient.Services
+{
+    public sealed class FileReader : IReader
+    {
+        public string Read(string path)
+        {
+            string text = string.Empty;
+            using(StreamReader stream = File.OpenText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path)))
+            {
+                text = stream.ReadToEnd();
+            }
+            return text;
+        }
+    }
+}
