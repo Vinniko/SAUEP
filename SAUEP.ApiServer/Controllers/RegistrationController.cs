5e21eae20e3ba489a372d87cc50b8f6fc9ffce23
Vinniko <vinnik_21@bk.ru>
2020-11-18 22:07:52 +0300

    Create RegistrationController commit
    Будет создан контроллер регистрации RegistrationController

---
+++ SAUEP.ApiServer/Controllers/RegistrationController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SAUEP.ApiServer.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using SAUEP.ApiServer.Models;
+
+namespace SAUEP.ApiServer.Controllers
+{
+    [Produces("application/json")]
+    public class RegistrationController : ControllerBase
+    {
+        #region Constructors
+
+        public RegistrationController(IGuardian guardian,  ILogger logger, IRegistration registration, IParser jsonParser)
+        {
+            _guardian = guardian;
+            _registration = registration;
+            _jsonParser = jsonParser;
+            _logger = logger;
+        }
+
+        #endregion
+
+
+
+        #region Http Methods
+
+        [Authorize(Roles = "Администратор")]
+        [HttpPost("/reg")]
+        public IActionResult Reg(string login, string password, string email, string role = "Пользователь")
+        {
+            _logger.Logg($"Попытка регистрации аккаунта {login} с ip адресса: {Request.HttpContext.Connection.RemoteIpAddress}");
+            var userModel = new UserModel(login, password, email);
+            if (_guardian.Secure(() => _registration.Registration(userModel)).Value)
+            {
+                _logger.Logg($"Аккаунт {login} зарегестрирован с ip адресса: {Request.HttpContext.Connection.RemoteIpAddress}");
+                return Ok();
+            }
+            else
+            {
+                _logger.Logg($"Попытка регистрации существующего {login} с ip адресса: {Request.HttpContext.Connection.RemoteIpAddress}");
+                return BadRequest("User exsists");
+            }
+                
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        IGuardian _guardian;
+        IRegistration _registration;
+        IParser _jsonParser;
+        ILogger _logger;
+
+        #endregion
+
+    }
+}
