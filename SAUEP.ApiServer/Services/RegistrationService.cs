591014a175c128f7eb16788d834c95327f823890
Vinniko <vinnik_21@bk.ru>
2020-11-18 21:56:07 +0300

    Create RegistrationService commit
    Будет создан сервис регистрации RegistrationService

---
+++ SAUEP.ApiServer/Services/RegistrationService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SAUEP.ApiServer.Interfaces;
+using SAUEP.ApiServer.Models;
+using SAUEP.ApiServer.Repositories;
+
+namespace SAUEP.ApiServer.Services
+{
+    public class RegistrationService : IRegistration
+    {
+        #region Constructors
+
+        public RegistrationService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        #endregion
+
+
+
+        #region Main Logic
+
+        public bool Registration(UserModel userModel)
+        {
+            foreach(var user in _userRepository.Get<UserModel>())
+            {
+                if(user.Login.Equals(userModel.Login))
+                    return false;
+            }
+            _userRepository.Set(userModel);
+            return true;
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        private UserRepository _userRepository;
+
+        #endregion
+    }
+}
