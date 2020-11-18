b19adfbfd5f523f1ba4f087e7446c4ae0fbbe28c
Vinniko <vinnik_21@bk.ru>
2020-11-18 22:12:37 +0300

    Config startup and project files
    Все сервисы будут зарегестрированы в DI методом ConfigureServices

--- SAUEP.ApiServer/Startup.cs
+++ SAUEP.ApiServer/Startup.cs
@@ -1,39 +1,63 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SAUEP.ApiServer.Configs;
+using SAUEP.ApiServer.Interfaces;
+using SAUEP.ApiServer.Services;
+using SAUEP.ApiServer.Connections;
+using SAUEP.ApiServer.Repositories;
 
 namespace SAUEP.ApiServer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidIssuer = AuthOptions.ISSUER,
+                            ValidateAudience = true,
+                            ValidAudience = AuthOptions.AUDIENCE,
+                            ValidateLifetime = true,
+                            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                            ValidateIssuerSigningKey = true,
+                        };
+                    });
+            services.AddControllersWithViews();
+            services.AddSingleton<IConnection, DataBaseConnection>();
+            services.AddSingleton<IParser, JSONParser>();
+            services.AddSingleton<IReader, FileReader>();
+            services.AddSingleton<IGuardian, Guardian>();
+            services.AddSingleton<ILogger, Logger>();
+            services.AddSingleton<IAuthorization, AuthorizationService>();
+            services.AddSingleton<IRegistration, RegistrationService>();
+            services.AddSingleton<UserRepository, UserRepository>();
+            services.AddSingleton<DeviceGroupRepository, DeviceGroupRepository>();
+            services.AddSingleton<DeviceRepository, DeviceRepository>();
+            services.AddSingleton<PollRepository, PollRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
