e8d4424038d233f10aaa1153611f07406fd5da20
Vinniko <vinnik_21@bk.ru>
2020-11-18 22:05:44 +0300

    Create DeviceGroupModel commit
    Будет создан класс-модель DeviceGroupModel

---
+++ SAUEP.ApiServer/Models/DeviceGroupModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SAUEP.ApiServer.Interfaces;
+
+namespace SAUEP.ApiServer.Models
+{
+    public sealed class DeviceGroupModel : IModel
+    {
+        #region Constructors
+
+        public DeviceGroupModel(string title, int id = 0)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        #endregion
+    }
+}
