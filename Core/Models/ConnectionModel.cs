3c846025da1bceaafb2cd8217fe19ca3b483bb30
Vinniko <vinnik_21@bk.ru>
2020-12-01 13:35:40 +0300

    Create ConnectionModel commit
    Будет создана модель соединения с сервером ConnectionModel

---
+++ Core/Models/ConnectionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAUEP.Core.Interfaces;
+
+namespace SAUEP.Core.Models
+{
+    public class ConnectionModel : IModel
+    {
+        public string ConnectionUrl { get; set; }
+    }
+}
