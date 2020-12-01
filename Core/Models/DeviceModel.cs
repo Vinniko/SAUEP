65487d699446b84d631ce0626429c11fe8926ffa
Vinniko <vinnik_21@bk.ru>
2020-12-01 13:34:23 +0300

    Create DeviceModel commit
    Будет создана модель устройства DeviceModel

---
+++ Core/Models/DeviceModel.cs
@@ -0,0 +1,44 @@
+﻿using SAUEP.Core.Interfaces;
+
+namespace SAUEP.Core.Models
+{
+    public sealed class DeviceModel : IModel
+    {
+        #region Constructors
+
+        public DeviceModel(string deviceGroup, string serial, string title, string ip, string port, bool status, double maxPower, double minPower, double maxElecticityConsumption, double minElecticityConsumption, int id = 0)
+        {
+            Id = id;
+            DeviceGroup = deviceGroup;
+            Serial = serial;
+            Title = title;
+            Ip = ip;
+            Port = port;
+            Status = status;
+            MaxPower = maxPower;
+            MinPower = minPower;
+            MaxElecticityConsumption = maxElecticityConsumption;
+            MinElecticityConsumption = minElecticityConsumption;
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        public int Id { get; set; }
+        public string DeviceGroup { get; set; }
+        public string Serial { get; set; }
+        public string Title { get; set; }
+        public string Ip { get; set; }
+        public string Port { get; set; }
+        public bool Status { get; set; }
+        public double MaxPower { get; set; }
+        public double MinPower { get; set; }
+        public double MaxElecticityConsumption { get; set; }
+        public double MinElecticityConsumption { get; set; }
+
+        #endregion
+    }
+}
