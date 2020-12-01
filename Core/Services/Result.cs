a29977199b96aa1b6756384dbb408bc5972f547b
Vinniko <vinnik_21@bk.ru>
2020-12-01 13:32:26 +0300

    Delete extra usage in services commit
    Будут удалены лишние импорты библиотек в сервисах: Guardian, Logger, Result

--- Core/Services/Result.cs
+++ Core/Services/Result.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Core.Services
+namespace SAUEP.Core.Services
 {
     public class Result
     {
