09633b2e689bbe522345c2b0f8f2fc191314171c
Vinniko <vinnik_21@bk.ru>
2020-12-01 13:36:28 +0300

    Create ServerConnection commit
    Будет создан класс для установки соединения с сервером

---
+++ Core/Connections/ServerConnection.cs
@@ -0,0 +1,30 @@
+﻿using SAUEP.Core.Interfaces;
+using SAUEP.Core.Models;
+
+namespace SAUEP.Core.Connections
+{
+    public class ServerConnection : IModel, IConnection
+    {
+        #region Constructors
+
+        public ServerConnection(IReader fileReader, IParser jsonParser)
+        {
+            _fileReader = fileReader;
+            _jsonParser = jsonParser;
+            ConnectionUrl = _jsonParser.Pars<ConnectionModel>(_fileReader.Read(_connectionFile)).ConnectionUrl;
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        public string ConnectionUrl { get; private set; }
+        private IReader _fileReader;
+        private IParser _jsonParser;
+        private string _connectionFile = @"Connections\Connection.json";
+
+        #endregion
+    }
+}
