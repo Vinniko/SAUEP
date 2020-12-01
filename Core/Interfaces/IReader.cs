ba3c260bbad2254189c4eea87447866aa3d13adf
Vinniko <vinnik_21@bk.ru>
2020-12-01 13:30:53 +0300

    Delete extra usage commit
    Будут удалены импорты лишних библиотек в интерфейсы

--- Core/Interfaces/IReader.cs
+++ Core/Interfaces/IReader.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.Interfaces
+﻿namespace SAUEP.Core.Interfaces
 {
     public interface IReader
     {
-        string Read();
+        string Read(string path);
     }
 }
