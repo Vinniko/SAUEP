039079e68dcaa1f7326b86a1282f44e0b2aad5c5
Vinniko <vinnik_21@bk.ru>
2020-12-06 11:02:16 +0300

    Create exceptions commit
    Будут созданы классы-исключения: AuthorizationException, RegistrationException, TokenLifeException

---
+++ Core/Exceptions/AuthorizationExcepton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SAUEP.Core.Exceptions
+{
+    public sealed class AuthorizationExcepton : Exception
+    {
+        public AuthorizationExcepton(string message)
+        : base(message)
+        { }
+    }
+}
---
+++ Core/Exceptions/RegistrationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SAUEP.Core.Exceptions
+{
+    public sealed class RegistrationException : Exception
+    {
+        public RegistrationException(string message)
+       : base(message)
+        { }
+    }
+}
