bcd041d74aa497308185fc3a892163cd9ed92109
Vinniko <vinnik_21@bk.ru>
2020-12-06 11:01:28 +0300

    Create IAuthorization interface commit
    Будет создан интерфейс IAuthorization

---
+++ Core/Interfaces/IAuthorization.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SAUEP.Core.Models;
+
+namespace SAUEP.Core.Interfaces
+{
+    public interface IAuthorization
+    {
+        Task<AuthorizationResponseModel> Authorize(string login, string password);
+    }
+}
