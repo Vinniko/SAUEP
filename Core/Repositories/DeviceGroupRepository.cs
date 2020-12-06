cca5896464bb6eea6c8716758427e6ef88669f99
Vinniko <vinnik_21@bk.ru>
2020-12-06 10:59:40 +0300

    Create repositories commit
    Будут созданы репозитории: DeviceGroupRepository, DeviceRepository, PollRepository

---
+++ Core/Repositories/DeviceGroupRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAUEP.Core.Interfaces;
+
+namespace SAUEP.Core.Repositories
+{
+    class DeviceGroupRepository //: IRepository
+    {
+    }
+}
