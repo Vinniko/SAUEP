b5d5d244a76462dea32c0c25d54c3780f49b7a50
Vinniko <vinnik_21@bk.ru>
2020-12-06 10:58:36 +0300

    Create AuthorizationService commit
    Будет создан сервис авторизации

---
+++ Core/Services/AuthorizationService.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using SAUEP.Core.Models;
+using SAUEP.Core.Interfaces;
+using SAUEP.Core.Exceptions;
+
+namespace SAUEP.Core.Services
+{
+    public sealed class AuthorizationService : IAuthorization
+    {
+        #region Constructors
+
+        public AuthorizationService(IConnection connection, IParser jsonParser)
+        {
+            _connection = connection;
+            _jsonParser = jsonParser;
+        }
+
+        #endregion
+
+
+
+        #region Main Logic
+
+        public async Task<AuthorizationResponseModel> Authorize(string login, string password)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create(_connection + "auth");
+                request.Method = "POST";
+                string data = $"username={login}&password={password}";
+                byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = byteArray.Length;
+                using (Stream dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                }
+                string token = string.Empty;
+                using (WebResponse response = await request.GetResponseAsync())
+                {
+                    using (Stream stream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            return _jsonParser.Pars<AuthorizationResponseModel>(reader.ReadToEnd());
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                HttpWebResponse httpResponse = (HttpWebResponse)ex.Response;
+                if((int)httpResponse.StatusCode == 400)
+                    throw new AuthorizationExcepton("Неправильный логин или пароль");
+                else throw new AuthorizationExcepton("Неизвестная ошибка авторизации");
+            }
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        private IConnection _connection;
+        private IParser _jsonParser;
+
+        #endregion
+    }
+}
