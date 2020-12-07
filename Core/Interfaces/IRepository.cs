2610ba8e1efd344c94b2ab353640613bc77d78d5
Vinniko <vinnik_21@bk.ru>
2020-12-07 16:27:23 +0300

    Fix IRepository commit
    void будет изменено на Task

--- Core/Interfaces/IRepository.cs
+++ Core/Interfaces/IRepository.cs
@@ -5,10 +5,10 @@ namespace SAUEP.Core.Interfaces
 {
     public interface IRepository
     {
-        void Set<T>(T data, string token);
+        Task Set<T>(T data, string token);
         Task<ICollection<T>> Get<T>(string token);
         Task<IModel> GetById(int id, string token);
-        void Update<T>(int id, T data, string token);
-        void Remove<T>(int id, string token);
+        Task Update<T>(int id, T data, string token);
+        Task Remove<T>(int id, string token);
     }
 }
