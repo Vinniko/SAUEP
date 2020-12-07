180cef5470b6347d5095d34e15412293a49ef999
Vinniko <vinnik_21@bk.ru>
2020-12-07 16:01:22 +0300

    Create JsonParser service commit
    В проекте Core будет создан сервис JsonParser

---
+++ Core/Services/JsonParser.cs
@@ -0,0 +1,21 @@
+﻿using SAUEP.Core.Interfaces;
+using System.Text.Json;
+
+namespace SAUEP.Core.Services
+{
+    public sealed class JsonParser : IParser
+    {
+        #region Main Logic
+
+        public T Pars<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+        public string Depars<T>(T data)
+        {
+            return JsonSerializer.Serialize<T>(data);
+        }
+
+        #endregion
+    }
+}
