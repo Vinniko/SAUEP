81086315cb1b05c1487f2a415b47fcaeafe6d59c
Vinniko <vinnik_21@bk.ru>
2021-03-29 14:14:50 +0300

    Create UsersFilter and UsersSorter commit

---
+++ Core/Services/UsersFilter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SAUEP.Core.Interfaces;
+using SAUEP.Core.Models;
+using SAUEP.Core.Exceptions;
+
+namespace SAUEP.Core.Services
+{
+    public sealed class UsersFilter : IFilter
+    {
+        #region Main Logic
+
+        public ICollection<T> Filtering<T>(ICollection<T> list)
+        {
+            return list;
+        }
+
+        public ICollection<T> Filtering<T>(ICollection<T> list, string key, string filter)
+        {
+            ICollection<T> filterCollection = new List<T>();
+            switch (key)
+            {
+                case "Login":
+                    (filterCollection as List<UserModel>).AddRange(from model in list as List<UserModel>
+                                                                            where model.Login.Contains(filter)
+                                                                            select model);
+                    return filterCollection;
+                case "Email":
+                    (filterCollection as List<UserModel>).AddRange(from model in list as List<UserModel>
+                                                                            where model.Email.Contains(filter)
+                                                                            select model);
+                    return filterCollection;
+                case "Role":
+                    (filterCollection as List<UserModel>).AddRange(from model in list as List<UserModel>
+                                                                   where model.Role.Contains(filter)
+                                                                   select model);
+                    return filterCollection;
+                default: throw new BadKeyException("Такого ключа фильтрации не существует: " + key);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Core/Services/UsersSorter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAUEP.Core.Interfaces;
+using SAUEP.Core.Models;
+using SAUEP.Core.Exceptions;
+
+namespace SAUEP.Core.Services
+{
+    public sealed class UsersSorter : ISorter
+    {
+        #region Main Logic
+
+        public ICollection<T> Sort<T>(ICollection<T> collection, string key, bool direction = true)
+        {
+            switch (key)
+            {
+                case "Login":
+                    if (direction)
+                    {
+                        ICollection<T> sortedCollection = new List<T>();
+                        (sortedCollection as List<UserModel>).AddRange((collection as List<UserModel>).OrderBy(k => k.Login));
+                        return sortedCollection;
+                    }
+                    else
+                    {
+                        ICollection<T> sortedCollection = new List<T>();
+                        (sortedCollection as List<UserModel>).AddRange((collection as List<UserModel>).OrderByDescending(k => k.Login));
+                        return sortedCollection;
+                    }
+                case "Email":
+                    if (direction)
+                    {
+                        ICollection<T> sortedCollection = new List<T>();
+                        (sortedCollection as List<UserModel>).AddRange((collection as List<UserModel>).OrderBy(k => k.Email));
+                        return sortedCollection;
+                    }
+                    else
+                    {
+                        ICollection<T> sortedCollection = new List<T>();
+                        (sortedCollection as List<UserModel>).AddRange((collection as List<UserModel>).OrderByDescending(k => k.Email));
+                        return sortedCollection;
+                    }
+                case "Role":
+                    if (direction)
+                    {
+                        ICollection<T> sortedCollection = new List<T>();
+                        (sortedCollection as List<UserModel>).AddRange((collection as List<UserModel>).OrderBy(k => k.Role));
+                        return sortedCollection;
+                    }
+                    else
+                    {
+                        ICollection<T> sortedCollection = new List<T>();
+                        (sortedCollection as List<UserModel>).AddRange((collection as List<UserModel>).OrderByDescending(k => k.Role));
+                        return sortedCollection;
+                    }
+                default: throw new BadKeyException("Такого ключа сортировки не существует: " + key);
+            }
+
+        }
+
+        #endregion
+    }
+}
