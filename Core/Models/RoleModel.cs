6ac639bfab311c30c6a25ec855d06225280ea43c
Vinniko <vinnik_21@bk.ru>
2021-03-29 14:13:23 +0300

    Create RoleModel and RoleRepository in Core project commit

---
+++ Core/Models/RoleModel.cs
@@ -0,0 +1,29 @@
+﻿using SAUEP.Core.Interfaces;
+
+namespace SAUEP.Core.Models
+{
+    public sealed class RoleModel : IModel
+    {
+        #region Constructros
+        public RoleModel()
+        {
+
+        }
+        public RoleModel(string title, int id = 0)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        #endregion
+    }
+}
---
+++ Core/Repositories/RoleRepository.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using SAUEP.Core.Interfaces;
+using SAUEP.Core.Models;
+using SAUEP.Core.Exceptions;
+using SAUEP.Core.Connections;
+
+namespace SAUEP.Core.Repositories
+{
+    public sealed class RoleRepository : IRepository
+    {
+        #region Constructors
+
+        public RoleRepository(IConnection connection, IParser jsonParser)
+        {
+            _connection = connection;
+            _jsonParser = jsonParser;
+        }
+
+        #endregion
+
+
+
+        #region Main Logic
+
+        public async Task Set<T>(T data, string token)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create((_connection as ServerConnection).ConnectionUrl + $"api/role/setRole?title={(data as RoleModel).Title}");
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = 0;
+                request.Headers.Add("Authorization", "Bearer " + token);
+                WebResponse response = await request.GetResponseAsync();
+            }
+            catch (WebException ex)
+            {
+                HttpWebResponse httpResponse = (HttpWebResponse)ex.Response;
+                if ((int)httpResponse.StatusCode == 401)
+                    throw new TokenLifetimeException("Время жизни токена авторизации истекло");
+                else throw new SetPollException("Неизвестная ошибка создания роли");
+            }
+        }
+
+        public async Task<ICollection<T>> Get<T>(string token)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create((_connection as ServerConnection).ConnectionUrl + "api/role/getRoles");
+                request.Method = "GET";
+                request.Headers.Add("Authorization", "Bearer " + token);
+                using (WebResponse response = await request.GetResponseAsync())
+                {
+                    using (Stream stream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            return _jsonParser.Pars<ICollection<T>>(reader.ReadToEnd());
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                HttpWebResponse httpResponse = (HttpWebResponse)ex.Response;
+                if ((int)httpResponse.StatusCode == 401)
+                    throw new TokenLifetimeException("Время жизни токена авторизации истекло");
+                else throw new GetPollsException("Неизвестная ошибка получения списка ролей, с кодом: " + ((int)httpResponse.StatusCode).ToString());
+            }
+        }
+
+        public async Task<IModel> GetById(int id, string token)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create((_connection as ServerConnection).ConnectionUrl + $"api/role/getRole?id={id}");
+                request.Method = "GET";
+                request.Headers.Add("Authorization", "Bearer " + token);
+                using (WebResponse response = await request.GetResponseAsync())
+                {
+                    using (Stream stream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            return _jsonParser.Pars<RoleModel>(reader.ReadToEnd());
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                HttpWebResponse httpResponse = (HttpWebResponse)ex.Response;
+                if ((int)httpResponse.StatusCode == 401)
+                    throw new TokenLifetimeException("Время жизни токена авторизации истекло");
+                else throw new GetPollException("Неизвестная ошибка получения роли, с кодом: " + ((int)httpResponse.StatusCode).ToString());
+            }
+        }
+
+        public async Task Update<T>(int id, T data, string token)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create((_connection as ServerConnection).ConnectionUrl + $"api/role/updateRole?id={id}&title={(data as RoleModel).Title}");
+                request.Method = "PUT";
+                request.ContentLength = 0;
+                request.Headers.Add("Authorization", "Bearer " + token);
+                WebResponse response = await request.GetResponseAsync();
+            }
+            catch (WebException ex)
+            {
+                HttpWebResponse httpResponse = (HttpWebResponse)ex.Response;
+                if ((int)httpResponse.StatusCode == 401)
+                    throw new TokenLifetimeException("Время жизни токена авторизации истекло");
+                else throw new UpdatePollException("Неизвестная ошибка обновления роли");
+            }
+        }
+
+        public async Task Remove<T>(int id, string token)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create((_connection as ServerConnection).ConnectionUrl + $"api/role/deleteRole?id={id}");
+                request.Method = "DELETE";
+                request.Headers.Add("Authorization", "Bearer " + token);
+                WebResponse response = await request.GetResponseAsync();
+            }
+            catch (WebException ex)
+            {
+                HttpWebResponse httpResponse = (HttpWebResponse)ex.Response;
+                if ((int)httpResponse.StatusCode == 400)
+                    throw new RemovePollException("Такой роли не существует");
+                else if ((int)httpResponse.StatusCode == 401)
+                    throw new TokenLifetimeException("Время жизни токена авторизации истекло");
+                else throw new RemovePollException("Неизвестная ошибка удаления роли");
+            }
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        private IConnection _connection;
+        private IParser _jsonParser;
+
+        #endregion
+    }
+}
