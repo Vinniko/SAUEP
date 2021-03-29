e2e01edca89d6ee4c07fbb9184af0cc3de5a4a02
Vinniko <vinnik_21@bk.ru>
2021-03-29 13:58:23 +0300

    Update DeviceClient main class commit
    Будет обновлено разбиение на потоки
    Будет добавлена синхронизация потоков

--- SAUEP.DeviceClient/Program.cs
+++ SAUEP.DeviceClient/Program.cs
@@ -19,20 +19,30 @@ namespace SAUEP.DeviceClient
             ResultGenerator generator = guardian.Secure(()=>container.Resolve<ResultGenerator>()).Value;
             guardian.Secure(() => generator.AddObserver(socketWriter as SocketWriter));
             IRepository deviceRepository = guardian.Secure(() => container.Resolve<IRepository>()).Value;
+            object locker = new object();
             while (true)
             {
-                IModel socket = guardian.Secure(() => new SocketModel()).Value;
-                (socketWriter as SocketWriter).Socket = socket as SocketModel;
                 ICollection<Thread> threads = new List<Thread>();
                 foreach (var device in deviceRepository.Get<DeviceModel>())
                 {
-                    threads.Add(new Thread(new ParameterizedThreadStart(generator.Generate)));
-                    threads.ElementAt(threads.Count - 1).Start(device);
+                    if (!device.Status) continue;
+                    threads.Add(new Thread(new ThreadStart(() =>
+                    {
+                        lock (locker)
+                        {
+                            IModel socket = guardian.Secure(() => new SocketModel()).Value;
+                            (socketWriter as SocketWriter).Socket = socket as SocketModel;
+                            generator.Generate(device);
+                        }
+                    }
+                    ))); 
+                    threads.ElementAt(threads.Count - 1).Start();
                 }
                 foreach (var thread in threads)
                 {
                     thread.Join();
                 }
+                Thread.Sleep(3000);
             }
         }
     }
