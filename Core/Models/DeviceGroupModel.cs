653e312b258b84f49d38ebd87c409243fb50a799
Vinniko <vinnik_21@bk.ru>
2021-03-29 13:46:37 +0300

    Add device collection in DEviceGroupModel commit

--- Core/Models/DeviceGroupModel.cs
+++ Core/Models/DeviceGroupModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using SAUEP.Core.Interfaces;
 
 namespace SAUEP.Core.Models
@@ -9,15 +6,16 @@ namespace SAUEP.Core.Models
     public sealed class DeviceGroupModel : IModel
     {
         #region Constructors
-        
+
         public DeviceGroupModel()
         {
-
+            DeviceModels = new ObservableCollection<DeviceModel>();
         }
         public DeviceGroupModel(string title, int id = 0)
         {
             Id = id;
             Title = title;
+            DeviceModels = new ObservableCollection<DeviceModel>();
         }
 
         #endregion
@@ -28,6 +26,7 @@ namespace SAUEP.Core.Models
 
         public int Id { get; set; }
         public string Title { get; set; }
+        public ObservableCollection<DeviceModel> DeviceModels {get; set;}
 
         #endregion
     }
