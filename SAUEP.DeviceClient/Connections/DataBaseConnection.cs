4bd70c3dc2e214ba4f42689ca05d76e07dfbe78f
Vinniko <vinnik_21@bk.ru>
2021-04-10 09:10:35 +0300

    DeviceClient: refactoring DataBaseConnection
    Логика старта соединения будет вынесена в отдельный метод
    Свойство класса Connection будет вынесено в отдельный регион

--- SAUEP.DeviceClient/Connections/DataBaseConnection.cs
+++ SAUEP.DeviceClient/Connections/DataBaseConnection.cs
@@ -10,9 +10,20 @@ namespace SAUEP.DeviceClient.Connections
 
         public DataBaseConnection(IReader reader, IParser jsonParser)
         {
-            _reader = reader;
             _parser = jsonParser;
-            var connection = _parser.Pars<ConnectionModel>(reader.Read(_connectionFile));
+            _reader = reader;
+            ConnectionEnable();
+        }
+
+        #endregion
+
+
+
+        #region Main Logic
+
+        private void ConnectionEnable()
+        {
+            var connection = _parser.Pars<ConnectionModel>(_reader.Read(_connectionFile));
             Connection = new NpgsqlConnection(string.Format(_connection, (connection as ConnectionModel).Host, (connection as ConnectionModel).Username, (connection as ConnectionModel).Password, (connection as ConnectionModel).Database));
             Connection.Open();
         }
@@ -31,13 +42,20 @@ namespace SAUEP.DeviceClient.Connections
 
 
 
+        #region Get/Set
+
+        public NpgsqlConnection Connection { get; set; }
+
+        #endregion
+
+
+
         #region Fields
 
         private const string _connection = "Host={0};Username={1};Password={2};Database={3}";
         private const string _connectionFile = @"Connections\Connection.json";
-        private IReader _reader;
         private IParser _parser;
-        public NpgsqlConnection Connection { get; set; }
+        private IReader _reader;
 
         #endregion
     }
